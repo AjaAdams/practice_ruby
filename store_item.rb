a55c87147599206e8ade2e6ceaa8e2a008282345
Aja <ajajhendrix@gmail.com>
2020-10-20 14:29:32 -0500

    initial commit

---
+++ store_item.rb
@@ -0,0 +1,5 @@
+shirt1 = { :type => "short-sleeve", :color => "black", :size => "small", :price => 15 }
+shirt2 = { :type => "long-sleeve", :color => "grey", :size => "medium", :price => 20 }
+shirt3 = { :type => "quarter-sleeve", :color => "red", :size => "large", :price => 30 }
+
+puts "Today I wore the #{shirt2[:color]} #{shirt2[:type]} shirt in a #{shirt2[:size]} and it cost me #{shirt2[:price]} dollars."
